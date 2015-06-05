afc8e4940a9ce891cc364dab693c18234b1a77d1
Ian on MacBook Pro <imacd20@gmail.com>
2015-06-05 09:49:12 -0700

    Add atom setup to setup script.

--- setup.sh
+++ setup.sh
@@ -12,6 +12,26 @@ curl https://raw.githubusercontent.com/matchbookmac/dotfiles/master/.bash_profil
 source ~/.bash_profile
 
 echo ''
+echo "Setting up Atom prefs"
+apm help > test 2>&1
+apmtest=$(<~/test)
+[[ $vartest =~ 'Atom Package Manager' ]] && installed=true || installed=false
+rm ~/test
+if [$installed]
+then
+  echo 'atom editor is installed'
+  apm install seti-syntax
+  apm install seti-ui
+  apm install open-in-browser
+  apm install toggle-quotes
+  echo 'atom setup complete'
+else
+  echo 'atom editor is not installed'
+  echo 'install at https://github.com/atom/atom/releases/'
+fi
+
+
+echo ''
 echo "Pulling down scaffolds and scripts"
 
 git clone https://github.com/matchbookmac/shell_scripts.git
