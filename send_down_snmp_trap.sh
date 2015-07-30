10549ccfc558895ee74f29da1ad7c1e9d33862b7
Ian on MacBook Pro <imacd20@gmail.com>
2015-07-30 13:02:28 -0700

    add scripts for sending snmp traps

---
+++ send_down_snmp_trap.sh
@@ -0,0 +1,1 @@
+snmptrap -v 1 -m +DOWN-BRIDGE-MIB -c public 192.168.1.187:3000 DOWN-BRIDGE-MIB::demotraps localhost 6 19 '' SNMPv2-MIB::sysLocation.0 s "DOWN signal"
---
+++ send_snmp_trap.sh
@@ -0,0 +1,1 @@
+snmptrap -v 1 -m +TRAP-TEST-MIB -c public 192.168.1.187:3000 TRAP-TEST-MIB::demotraps localhost 6 17 '' SNMPv2-MIB::sysLocation.0 s "SNMP Message from Ian"
