ca9071132605824db6917795a0191d68cf562768
Ian MacDonald <ianmacdonald@Ian-MacDonalds-MacBook-Pro.local>
2015-11-06 19:07:44 -0800

    Add snmp scripts

--- send_down_snmp_trap.sh
+++ send_down_snmp_trap.sh
@@ -1,1 +1,1 @@
-snmptrap -v 1 -m +DOWN-BRIDGE-MIB -c public 192.168.1.187:3000 DOWN-BRIDGE-MIB::demotraps localhost 6 19 '' SNMPv2-MIB::sysLocation.0 s "DOWN signal"
+snmptrap -v 1 -m +DOWN-BRIDGE-MIB -c public 192.168.1.123:3000 DOWN-BRIDGE-MIB::demotraps localhost 6 19 '' SNMPv2-MIB::sysLocation.0 s "DOWN signal"
--- send_up_snmp_trap.sh
+++ send_up_snmp_trap.sh
@@ -1,1 +1,1 @@
-snmptrap -v 1 -m +UP-BRIDGE-MIB -c public 192.168.1.187:3000 UP-BRIDGE-MIB::demotraps localhost 6 18 '' SNMPv2-MIB::sysLocation.0 s "UP signal"
+snmptrap -v 1 -m +UP-BRIDGE-MIB -c public 192.168.1.123:3000 UP-BRIDGE-MIB::demotraps localhost 6 18 '' SNMPv2-MIB::sysLocation.0 s "UP signal"
